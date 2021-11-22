8790d0da2b99d33bbd6c155be677cc35a91a3ece
Idris Dzhumanov <ijumanov@alfabank.kz>
2021-11-22 16:08:22 +0600

    check if request id header is not set

--- middleware_request_id.go
+++ middleware_request_id.go
@@ -9,7 +9,7 @@ import (
 func xRequestID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		reqID, ok := r.Context().Value(chiMiddleware.RequestIDKey).(string)
+		reqID, ok := ctx.Value(chiMiddleware.RequestIDKey).(string)
 		if ok {
 			w.Header().Set(chiMiddleware.RequestIDHeader, reqID)
 		}
